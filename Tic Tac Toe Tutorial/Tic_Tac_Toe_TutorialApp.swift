313d00eea361c85d7c859ab01654c91bd18e602e
Oncu <oohancan@gmail.com>
2022-05-03 15:44:39 +0300

    Initial Commit

---
+++ Tic Tac Toe Tutorial/Tic_Tac_Toe_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_TutorialApp.swift
+//  Tic Tac Toe Tutorial
+//
+//  Created by Oncu Ohancan on 3.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
