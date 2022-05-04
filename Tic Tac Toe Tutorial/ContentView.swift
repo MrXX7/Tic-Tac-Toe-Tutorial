f2cb3709d125763ab26981e6e58c90274deda963
Oncu <oohancan@gmail.com>
2022-05-04 18:37:24 +0300

    add state message and func decidewinner and checkmoves

--- Tic Tac Toe Tutorial/ContentView.swift
+++ Tic Tac Toe Tutorial/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     @State var moves: [String] = Array(repeating: "", count: 9)
     @State var currentUser = false
+    @State var message = ""
+    @State var gameOver = false
     
     var body: some View {
         NavigationView {
@@ -21,7 +23,7 @@ struct ContentView: View {
                         Color.blue
                         
                         Color.white
-                            .opacity(moves[index] != "" ? 1 : 0)
+                            .opacity(moves[index] == "" ? 1 : 0)
                         
                         Text(moves[index])
                             .font(.system(size: 55))
@@ -30,11 +32,10 @@ struct ContentView: View {
                     }
                         .frame(width: width, height: width)
                         .cornerRadius(15)
-                        .rotation3DEffect(.init(degrees: moves[index]) !=
-                                          "" ? 180 : 0),
+                        .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0),
                 axis: (x: 0.0, y: 1.0, z: 0.0),
                 anchor: .center,
-                anchorz: 0.0,
+                anchorZ: 0.0,
                 perspective: 1.0)
                         .onTapGesture {
                             withAnimation(Animation.easeIn(duration: 0.15)) {
@@ -48,10 +49,50 @@ struct ContentView: View {
             }) .padding(15)
                 .preferredColorScheme(.dark)
                 .navigationTitle("Tic Tac Toe")
+                .onChange(of: moves, perform: { value in
+                 decideWinner()
+                })
         }
     }
     var width: CGFloat {
         return (UIScreen.main.bounds.width - 60) / 3
+    }
+    
+    func decideWinner() {
+        if checkMoves(player: "X") {
+            message = "Player X won the game!!"
+            self.gameOver.toggle()
+        } else if checkMoves(player: "O") {
+            message = "Player O won the game!!"
+            self.gameOver.toggle()
+        }
+    }
+    
+    func checkMoves(player: String) -> Bool {
+        
+//        Horizontal Check
+        for i in stride(from: 0, to: 9, by: 3) {
+            if moves[i] == player && moves[i + 1] == player && moves[i + 3] == player {
+                return true
+            }
+        }
+        
+//        Vertical Check
+        for i in 0...2 {
+            if moves[i] == player && moves[i + 3] == player && moves[i + 6] == player {
+                return true
+            }
+        }
+//        Diagonal Check
+        
+        if moves[0] == player && moves[5] == player && moves[8] == player {
+            return true
+        }
+        if moves[2] == player && moves[4] == player && moves[6] == player {
+            return true
+        }
+        
+        return false
     }
 }
 
