3ebb6d70e4ff4912a61a4d7eae0bde226de3be3b
Oncu <oohancan@gmail.com>
2022-05-04 12:02:33 +0300

    add state and gameplay

--- Tic Tac Toe Tutorial/ContentView.swift
+++ Tic Tac Toe Tutorial/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var currentUser = false
+    
     var body: some View {
         NavigationView {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(),
                 spacing: 15), count: 3), spacing: 15, content: {
                 ForEach(0..<9) { index in
-                    Color.white
+                    ZStack {
+                        Color.blue
+                        
+                        Color.white
+                            .opacity(moves[index] != "" ? 1 : 0)
+                        
+                        Text(moves[index])
+                            .font(.system(size: 55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(moves[index] == "" ? .black : .white)
+                    }
                         .frame(width: width, height: width)
-                        .cornerRadius(10)
+                        .cornerRadius(15)
+                        .rotation3DEffect(.init(degrees: moves[index]) !=
+                                          "" ? 180 : 0),
+                axis: (x: 0.0, y: 1.0, z: 0.0),
+                anchor: .center,
+                anchorz: 0.0,
+                perspective: 1.0)
+                        .onTapGesture {
+                            withAnimation(Animation.easeIn(duration: 0.15)) {
+                            if moves[index] == "" {
+                                moves[index] = currentUser ? "X" : "O"
+                                self.currentUser.toggle()
+                            }
+                        }
+                    }
                 }
             }) .padding(15)
                 .preferredColorScheme(.dark)
